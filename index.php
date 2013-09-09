840a600f1e32df721f7892e7217f8a42da5358ef
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-09 02:11:07 -0400

    Added image of myself and CSS styles

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 <body>
 <div class="wrapper">
   <h1>Andy Pearson</h1>
+  <img class="portrait" src="img/andy2009.jpg" width="243" height="288" alt="Andy Pearson's portrait">
   <p>Hi, I’m Andy Pearson from Brooklyn, NY.  I work as a freelance front-end web developer and am studying for an ALM IT, Information management systems.  Before my career in IT I was a paratrooper in the U. S. Army.  I speak Spanish and my hobbies include drawing and painting.</p>
   <p>I use Dreamweaver and sometimes Sublime Text.</p>
 </div><!-- Close class="wrapper" -->
