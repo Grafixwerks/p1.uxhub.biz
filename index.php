3987f29499867fcf3ce61b7ee81ed203a6f44b36
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-09 01:11:42 -0400

    Added wraper div and formatted HTML code

--- index.php
+++ index.php
@@ -3,18 +3,15 @@
 <head>
 <meta charset="utf-8">
 <title>CSCI E-15 | P 1</title>
-<link rel="stylesheet" type="text/css" href="css/main.css" media="screen>
+<link rel="stylesheet" type="text/css" href="css/main.css" media="screen" >
 </head>
 
 <body>
-
-<h1>Andy Pearson</h1>
-
-<p>Hi, I’m Andy Pearson from Brooklyn, NY.  I work as a freelance front-end web developer and am studying for an ALM IT, Information management systems.  Before my career in IT I was a paratrooper in the U. S. Army.  I speak Spanish and my hobbies include drawing and painting.</p> 
-
-<p>I use Dreamweaver and sometimes Sublime Text.</p>
-
-
+<div class="wrapper">
+  <h1>Andy Pearson</h1>
+  <p>Hi, I’m Andy Pearson from Brooklyn, NY.  I work as a freelance front-end web developer and am studying for an ALM IT, Information management systems.  Before my career in IT I was a paratrooper in the U. S. Army.  I speak Spanish and my hobbies include drawing and painting.</p>
+  <p>I use Dreamweaver and sometimes Sublime Text.</p>
+</div><!-- Close class="wrapper" -->
 
 </body>
 </html>
