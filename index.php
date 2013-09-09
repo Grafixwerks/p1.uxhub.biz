f91e58f0fb98d265c7d6609f63f933af5a3018eb
Andrew Pearson <Andy@Grafixwerks.com>
2013-09-09 00:59:55 -0400

    Added text

--- index.php
+++ index.php
@@ -6,5 +6,14 @@
 </head>
 
 <body>
+
+<h1>Andy Pearson</h1>
+
+<p>Hi, I’m Andy Pearson from Brooklyn, NY.  I work as a freelance front-end web developer and am studying for an ALM IT, Information management systems.  Before my career in IT I was a paratrooper in the U. S. Army.  I speak Spanish and my hobbies include drawing and painting.</p> 
+
+<p>I use Dreamweaver and sometimes Sublime Text.</p>
+
+
+
 </body>
 </html>
